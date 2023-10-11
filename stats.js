c1272802426617b0cb6b9086c5a7e571ba2399fd
杨世浩 <yangshihao@bhb.tv>
2023-10-11 17:37:51 +0800

    style:样式调整

--- stats.js
+++ stats.js
@@ -1,77 +1,100 @@
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.storage.local.get(null, (result) => {
-    const container = document.getElementById('siteStatsContainer');
+/**解析csv数据 */
+function parseCSV(data) {
+  let parsedData = [];
+  let rows = data.trim().split('\n');
+  let headers = rows[0].split(',');
 
-    const dataForDate = result['2023-09-08'] || {};
-
-    if (!container) {
-      console.error('siteStatsContainer not found!');
-      return;
+  for (let i = 1; i < rows.length; i++) {
+    let obj = {};
+    let currentRow = rows[i].split(',');
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j].trim()] = currentRow[j].trim();
     }
+    parsedData.push(obj);
+  }
+  return parsedData;
+}
 
-    for (let url in dataForDate) {
-      const card = document.createElement('div');
-      card.className = 'site-card';
+// 将数据存储到chrome.storage
+function saveToStorage(data) {
+  chrome.storage.local.set({ importedData: data });
+}
 
-      const domain = document.createElement('h2');
-      domain.textContent = url;
+// 从chrome.storage获取数据
+function getFromStorage(callback) {
+  chrome.storage.local.get('importedData', function (result) {
+    callback(result.importedData);
+  });
+}
 
-      const timeSpent = document.createElement('p');
-      timeSpent.textContent = `Time Spent: ${(
-        dataForDate[url].totalTime / 60000
-      ).toFixed(2)} minutes`;
+// 渲染数据到UI
+function renderData(data) {
+  const container = document.getElementById('siteStatsContainer');
+  if (!container) {
+    console.error('siteStatsContainer not found!');
+    return;
+  }
 
-      const openCountValue =
-        (dataForDate[url] && dataForDate[url].openCount) || 0;
-      const openCount = document.createElement('p');
-      openCount.textContent = `Opened: ${openCountValue} times`;
+  // 清空之前的数据
+  container.innerHTML = '';
 
-      card.appendChild(domain);
-      card.appendChild(timeSpent);
-      card.appendChild(openCount);
+  // 最多显示的数据条数
+  const maxDataToShow = 10;
 
-      container.appendChild(card);
-    }
+  data.slice(0, maxDataToShow).forEach((item) => {
+    const card = document.createElement('div');
+    card.className = 'site-card';
+
+    const domain = document.createElement('h2');
+    domain.textContent = item.url;
+
+    const timeSpent = document.createElement('p');
+    timeSpent.textContent = `Time Spent: ${(
+      item['time in seconds'] / 60
+    ).toFixed(2)} minutes`;
+
+    const openCount = document.createElement('p');
+    openCount.textContent = `Opened: ${item.openCount || 0} times`;
+
+    card.appendChild(domain);
+    card.appendChild(timeSpent);
+    card.appendChild(openCount);
+    container.appendChild(card);
   });
 
-  chrome.storage.local.get(null, (result) => {
-    const labels = [];
-    const data = [];
-    const dataForDate = result['2023-09-08'];
-
-    for (let url in dataForDate) {
-      labels.push(url);
-      data.push(dataForDate[url].totalTime / 60000); // Convert to minutes
-    }
-
-    const ctx = document.getElementById('usageChart').getContext('2d');
-    new Chart(ctx, {
-      type: 'pie',
-      title: {
-        display: true,
-        text: '网页使用时长',
-      },
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            data: data,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              // ... Add more colors if needed
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              // ... Add more colors if needed
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
+  // 如果数据超过10条，显示"show more"按钮
+  if (data.length > maxDataToShow) {
+    const showMoreBtn = document.createElement('button');
+    showMoreBtn.textContent = 'Show More';
+    showMoreBtn.addEventListener('click', function () {
+      renderData(data.slice(maxDataToShow));
+      showMoreBtn.style.display = 'none'; // 隐藏按钮
     });
+    container.appendChild(showMoreBtn);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // 处理CSV文件导入
+  document
+    .getElementById('csvFileInput')
+    .addEventListener('change', function (e) {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+
+      reader.onload = function (event) {
+        const parsedData = parseCSV(event.target.result);
+        saveToStorage(parsedData);
+        renderData(parsedData);
+      };
+
+      reader.readAsText(file);
+    });
+
+  // 初始化：从storage获取数据并渲染
+  getFromStorage(function (data) {
+    if (data) {
+      renderData(data);
+    }
   });
 });
